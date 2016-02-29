00497d973e6e9426b04f6faedc7eef697b0ccc46
Alex Powelson <alexpow8@gmail.com>
2016-02-29 16:19:56 -0700

    unit 8

---
+++ twitter.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Twitter Sign In for ebooks</title>
+</head>
+
+<body>
+
+<?php
+echo "<h2>Simple Twitter API Test</h2>";
+?>
+
+</body>
+</html>
